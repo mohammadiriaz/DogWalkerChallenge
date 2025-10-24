e033d2237450c87b1f16cc4d39fa39d71e560e55
MohammadRiaz <mohammadr95@nycstudents.net>
2025-10-24 12:33:28 -0400

    Almost done

--- src/DogWalkCompany.java
+++ src/DogWalkCompany.java
@@ -1,4 +1,40 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-public class DogWalkCompany {
+public class DogWalkCompany
+{
+    private int[] schedule;
+    public DogWalkCompany()
+    {
+        schedule = new int[24];
+    }
+    public DogWalkCompany(int[] schedule) {
+        this.schedule = schedule;
+    }
+    public String toString()
+    {
+        String s = "";
+        for (int i = 0; i < schedule.length; i++)
+        {
+            s += i + " " + schedule[i] + "\n";
+        }
+        return s;
+    }
+    public int addDogs()
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.println("What hour?");
+        int hour = s.nextInt();
+        System.out.println("How many dogs?");
+        int dogs = s.nextInt();
+        schedule[hour] = dogs;
+        return hour;
+    }
+    public int numAvailableDogs(int hour)
+    {
+        return schedule[hour];
+    }
+    public void updateDogs(int hour, int numberDogsWalked)
+    {
+        schedule[hour] -= numberDogsWalked;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,48 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        {
+        }
+        DogWalkCompany c = new DogWalkCompany();
+        int hour = c.addDogs();
+        DogWalker w = new DogWalker(3, c);
+        for (int i = 7; i < 11; i++) {
+            c.addDogs();
+        }
+        System.out.println(w.walkDogs(hour) + " dogs walked.");
+        System.out.println(c.numAvailableDogs(hour) + " dogs remaining.");
+        System.out.println(c);
+        System.out.println("$" + w.dogWalkShift(7, 10) + ".00 earned");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    }
+
+
+    public static int read() throws FileNotFoundException {
+        int money = 0;
+        DogWalkCompany[] companies = new DogWalkCompany[1000];
+        File f = new File("Companies.txt");
+        Scanner s = new Scanner(f);
+        int index = 0;
+        while (s.hasNextLine()) {
+            int[] dogs = new int[24];
+            for (int i = 0; i < dogs.length; i++)
+            {
+                dogs[i] = s.nextInt();
+            }
+                companies[index] = new DogWalkCompany(dogs);
+                index++;
+            }
+       DogWalker[] walkers = new DogWalker[(int)Math.pow(10, 4)];
+        f = new File("Walkers.txt");
+        s = new Scanner(f);
+        index = 0;
+        while (s.hasNextLine()) {
+            int dogs = s.nextInt();
+            DogWalkCompany company = companies[s.nextInt()];
+            walkers[index] = new
+        }
         }
     }
-}
