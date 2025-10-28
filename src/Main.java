90782e7c9e1683032a0ce62e4dca8b0eb7ffdf3c
MohammadRiaz <mohammadr95@nycstudents.net>
2025-10-28 12:13:16 -0400

    Finished Dog Walker Challenge.

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,9 @@ import java.io.File;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        {
-        }
-        DogWalkCompany c = new DogWalkCompany();
-        int hour = c.addDogs();
-        DogWalker w = new DogWalker(3, c);
-        for (int i = 7; i < 11; i++) {
-            c.addDogs();
-        }
-        System.out.println(w.walkDogs(hour) + " dogs walked.");
-        System.out.println(c.numAvailableDogs(hour) + " dogs remaining.");
-        System.out.println(c);
-        System.out.println("$" + w.dogWalkShift(7, 10) + ".00 earned");
-
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println(read());
     }
-
 
     public static int read() throws FileNotFoundException {
         int money = 0;
@@ -28,21 +15,23 @@ public class Main {
         int index = 0;
         while (s.hasNextLine()) {
             int[] dogs = new int[24];
-            for (int i = 0; i < dogs.length; i++)
-            {
+            for (int i = 0; i < dogs.length; i++) {
                 dogs[i] = s.nextInt();
             }
-                companies[index] = new DogWalkCompany(dogs);
-                index++;
-            }
-       DogWalker[] walkers = new DogWalker[(int)Math.pow(10, 4)];
+            companies[index] = new DogWalkCompany(dogs);
+            index++;
+        }
+        DogWalker[] walkers = new DogWalker[(int) Math.pow(10, 4)];
         f = new File("Walkers.txt");
         s = new Scanner(f);
         index = 0;
         while (s.hasNextLine()) {
             int dogs = s.nextInt();
             DogWalkCompany company = companies[s.nextInt()];
-            walkers[index] = new
+            walkers[index] = new DogWalker(dogs, company);
+            money += walkers[index].dogWalkShift(s.nextInt(), s.nextInt());
+            index++;
         }
-        }
+        return money;
     }
+}
